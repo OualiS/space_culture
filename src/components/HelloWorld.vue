68b1677830a4ba41d4055eb429b6c12e235b7208
hyverno <solo.hyverno@gmail.com>
2022-01-14 21:08:55 +0100

    add threejs

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,28 +1,53 @@
 <template>
-  <v-container>
-    <center>
-      <h1 :style="{color: $vuetify.theme.themes[theme].primary}">Space Culture</h1>      
-    </center>
-    <br>
-    <div>
-      <center>
-          <h2 :style="{color: $vuetify.theme.themes[theme].primary}">James Webb Tracker</h2>
-      </center>      
-      <hr class="hr1" :style="{color: $vuetify.theme.themes[theme].primary}">
+  <div>
+
+    <h1>hello world</h1>
+
+    <div id="canvas">
     </div>
-  </v-container>
+
+  </div>
 </template>
 
 <script>
-  export default {
-    name: 'HelloWorld',
-    computed:{
-      theme(){
-        return (this.$vuetify.theme.themes.dark) ? 'dark' : 'light'
-      }
+import * as THREE from 'three'
+export default {
+  data() {
+    return {
+      camera: null,
+      scene: null,
+      renderer: null,
+      mesh: null
+    }
+  },
+  methods: {
+    init: function() {
+			this.scene = new THREE.Scene();
+			this.camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+			this.renderer = new THREE.WebGLRenderer();
+			this.renderer.setSize( window.innerWidth, window.innerHeight );
+			document.body.appendChild( this.renderer.domElement );
+
+			let geometry = new THREE.BoxGeometry();
+			let material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+			this.mesh = new THREE.Mesh( geometry, material );
+			this.scene.add( this.mesh );
+
+			this.camera.position.z = 5;
     },
-    data: () => ({
-      
-    }),
-  }
+    animate: function() {
+        requestAnimationFrame(this.animate);
+        this.mesh.rotation.x += 0.01;
+        this.mesh.rotation.y += 0.02;
+        this.renderer.render(this.scene, this.camera);
+    }
+  },
+  mounted() {
+    this.init()
+    this.animate()
+  },
+}
+
+
 </script>
