16bf6119c31a0ea32a1d4c7efea4a130362a9454
OualiS <salah.57970@gmail.com>
2022-01-14 19:18:20 +0100

    main page

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
-  <v-app>
-    <v-app-bar
+  <v-app :style="{background: $vuetify.theme.themes[theme].background}">
+<!-- <v-app-bar
       app
       color="primary"
       dark
@@ -35,7 +35,7 @@
         <span class="mr-2">Latest Release</span>
         <v-icon>mdi-open-in-new</v-icon>
       </v-btn>
-    </v-app-bar>
+</v-app-bar> -->
 
     <v-main>
       <router-view/>
@@ -47,9 +47,31 @@
 
 export default {
   name: 'App',
-
+  computed:{
+    theme(){
+      return (this.$vuetify.theme.themes.dark) ? 'dark' : 'light'
+    }
+  },
+  created () {
+    console.log(this.$vuetify.theme.themes)
+  },
   data: () => ({
     //
   }),
 };
 </script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Trispace:wght@800&display=swap');
+
+h1,h2{
+  font-family: 'Trispace', sans-serif;
+}
+
+.hr1{
+    border: 0;
+    height: 1px;
+    background-image: linear-gradient(to right, rgba(113, 4, 163, 0), rgba(218, 43, 253, 0.75), rgba(0, 0, 0, 0));
+    
+}
+</style>
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,7 +1,25 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib/framework';
 
+import colors from 'vuetify/lib/util/colors'
+
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    theme: {
+        themes: {
+          light: {
+            primary: colors.purple,
+            secondary: colors.grey.darken1,
+            accent: colors.shades.black,
+            error: colors.red.accent3,
+            background: colors.grey.lighten5
+          },
+          dark: {
+            primary: colors.grey.lighten5,
+            secondary : colors.cyan,
+            background: colors.grey.darken4,
+          },
+        },
+      },
 });
